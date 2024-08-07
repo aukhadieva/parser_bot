3a7fe4b57060b8015d213a1534369cedd3a1209d
Olga Aukhadieva <sarole4ka@gmail.com>
2024-08-07 15:26:08 +0500

    Add calculate_avg_price func

--- services.py
+++ services.py
@@ -1,5 +1,10 @@
+import sqlite3
+
+import pandas as pd
 import requests
 from lxml import html
+
+from utils import db_path
 
 
 def fetch_price(url: str, xpath: str) -> str | None:
@@ -17,3 +22,24 @@ def fetch_price(url: str, xpath: str) -> str | None:
         return price_elements[0].text_content().strip()
 
     return None
+
+
+def calculate_avg_price() -> float:
+    """
+    Вычисляет среднюю цену товаров.
+
+    :return: Средняя цена в рублях
+    """
+    conn = sqlite3.connect(db_path)
+    df = pd.read_sql_query("SELECT * FROM products", conn)
+    prices = []
+
+    for index, row in df.iterrows():
+        price = fetch_price(row['url'], row['xpath'])
+        if price:
+            price = price.replace(' ', '').replace('₽', '').replace('₴', '')
+            prices.append(float(price))
+
+    avg_price = sum(prices) / len(prices) if prices else 0
+    conn.close()
+    return avg_price
