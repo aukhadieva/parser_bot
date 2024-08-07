3112bf73c708225100c3d57437661d4e094a5d77
Olga Aukhadieva <sarole4ka@gmail.com>
2024-08-07 10:43:26 +0500

    Add save_to_database func

--- database.py
+++ database.py
@@ -1,3 +1,4 @@
+import logging
 import sqlite3
 
 from utils import db_path
@@ -13,3 +14,27 @@ def setup_database() -> None:
     cursor.execute('''CREATE TABLE IF NOT EXISTS products (title TEXT, url TEXT, xpath TEXT)''')
     conn.commit()
     conn.close()
+
+
+def save_to_database(dataframe) -> None:
+    """
+    Сохраняет данные из DataFrame в базу данных SQLite.
+    :param dataframe: Pandas DataFrame, содержащий данные для сохранения.
+    :return:
+    """
+    try:
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+
+        for index, row in dataframe.iterrows():
+            cursor.execute("INSERT INTO products (title, url, xpath) VALUES (?, ?, ?)",
+                           (row['title'], row['url'], row['xpath']))
+
+        conn.commit()
+
+    except sqlite3.Error as error:
+        logging.error(f'Ошибка при сохранении в БД: {error}')
+
+    finally:
+        if conn:
+            conn.close()
