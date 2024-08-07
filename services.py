4550258e491f58a3f2c9ee9b586923cb7f891a00
Olga Aukhadieva <sarole4ka@gmail.com>
2024-08-07 19:29:40 +0500

    Update calculate_avg_price func

--- services.py
+++ services.py
@@ -1,4 +1,5 @@
 import sqlite3
+from urllib.parse import urlparse
 
 import pandas as pd
 import requests
@@ -24,7 +25,7 @@ def fetch_price(url: str, xpath: str) -> str | None:
     return None
 
 
-def calculate_avg_price() -> float:
+def calculate_avg_price() -> dict:
     """
     Вычисляет среднюю цену товаров.
 
@@ -32,14 +33,23 @@ def calculate_avg_price() -> float:
     """
     conn = sqlite3.connect(db_path)
     df = pd.read_sql_query("SELECT * FROM products", conn)
-    prices = []
+    site_prices = {}
 
     for index, row in df.iterrows():
         price = fetch_price(row['url'], row['xpath'])
         if price:
             price = price.replace(' ', '').replace('₽', '').replace('₴', '')
-            prices.append(float(price))
+            price_value = float(price)
+            hostname = urlparse(row['url']).netloc
 
-    avg_price = sum(prices) / len(prices) if prices else 0
+            if hostname not in site_prices:
+                site_prices[hostname] = {'total_price': 0, 'count': 0}
+
+            site_prices[hostname]['total_price'] += price_value
+            site_prices[hostname]['count'] += 1
+
+    avg_prices = {site: round(data['total_price'] / data['count'], 2) for site, data in site_prices.items()
+                  if data['count'] > 0}
+
     conn.close()
-    return avg_price
+    return avg_prices
